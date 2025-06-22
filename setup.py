6336ba8bb1db790e89e078fb773d02d4e8b35f40
Jakub Ciura <jakub.ciura@gmail.com>
2025-06-22 22:01:16 +0200

    change in cors - allowing front to connect with back (api)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://weather-frontend-bdwz.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
